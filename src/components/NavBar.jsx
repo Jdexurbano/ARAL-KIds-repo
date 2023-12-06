60c62ff4864cc484ac68e18222429f9e3f0afa79
Dexter Urbano <dexterurbano25@gmail.com>
2023-12-06 22:07:44 +0800

    fix responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,64 @@
+/** @format */
+
 import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
+import { HiBars3, HiMiniXMark } from "react-icons/hi2";
+import { useState, useEffect } from "react";
 function NavBar() {
+  const pages = [
+    { id: 1, address: "#Home", name: "Home" },
+    { id: 2, address: "#About", name: "About" },
+    { id: 3, address: "#Feature", name: "Feature" },
+    { id: 4, address: "#Contact", name: "Contact" },
+  ];
+  const [open, setOpen] = useState(false);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setInnerWidth(window.innerWidth);
+    innerWidth >= 900 && setOpen(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
   return (
     <>
-      <header className="w-full h-[80px]  pr-[100px] pl-[130px] flex justify-between items-center bg-primary z-10">
-        <img
-          src="/images/Website_Logo.png"
-          alt="ARAL-KIDS Logo"
-          className="w-[80px] h-[80px]"
-        />
-        <div className=" w-[600px] flex justify-around items-center font-poppins font-semibold">
-          <Link to={"#Home"} smooth>
-            Home
-          </Link>
-          <Link to={"#About"} smooth>
-            About
-          </Link>
-          <Link to={"#Feature"} smooth>
-            Feature
-          </Link>
-          <Link to={"#Contact"} smooth>
-            Contact
-          </Link>
+      <header className="z-10 bg-primary px-[20px] desktop:flex desktop:items-center desktop:px-[50px] large:px-[130px]">
+        <div className="flex items-center justify-between cursor-pointer w-full">
+          <img
+            src="/images/Website_Logo.png"
+            alt="ARAL-KIDS Logo"
+            className="w-[60px] h-[60px] large:w-[80px] large:h-[80px]"
+          />
+          {!open ? (
+            <HiBars3
+              className="h-[40px] w-[40px] desktop:hidden"
+              onClick={() => setOpen(!open)}
+            />
+          ) : (
+            <HiMiniXMark
+              className="h-[40px] w-[40px] desktop:hidden"
+              onClick={() => setOpen(!open)}
+            />
+          )}
+        </div>
+        <div
+          className={`${
+            !open && "hidden"
+          } desktop:inline desktop:w-[900px] large:w-[]`}
+        >
+          <ul className="font-semibold text-[18px] pl-[10px] desktop:flex desktop:justify-between">
+            {pages.map((page) => {
+              return (
+                <li key={page.id} className="mt-2">
+                  <Link to={page.address} smooth onClick={() => setOpen(!open)}>
+                    {page.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </header>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,9 @@ function Home() {
         id="Home"
         className="desktop:ml-[50px] large:ml-[130px] desktop:h-screen"
       >
-        <div className="desktop:flex desktop:justify-between desktop:items-start">
+        <div className="desktop:flex desktop:justify-between desktop:items-start pt-[40px]">
           <div className="flex flex-col items-center desktop:items-start">
-            <div className="text-center desktop:text-left font-extrabold text-[40px] desktop:text-[55px] large:text-[65px] text-slate-100">
+            <div className="text-center desktop:text-left font-extrabold text-[40px] desktop:text-[55px] large:text-[70px] text-slate-100 leading-[1.20] mb-[20px]">
               <h1>
                 <span className="text-sixth">A</span>ugmented
               </h1>
@@ -21,15 +21,15 @@ function Home() {
                 <span className="text-fifth">A</span>ndroid-Based
               </h1>
               <h1>
-                <span className="text-nine">l</span>earning for
+                <span className="text-nine">L</span>earning for
               </h1>
               <h1 className=" text-seven">Kids</h1>
             </div>
             <div className="mb-[10px] font-semibold desktop:font-bold">
-              <button className="py-[11px] desktop:py-[21px] px-[16px] desktop:px-[32px] bg-fourth rounded-[10px] mr-[5px]">
+              <button className="py-[11px] desktop:py-[21px] px-[16px] desktop:px-[32px] bg-fourth rounded-[15px] mr-[5px]">
                 <h1 className="text-slate-200">Download Now</h1>
               </button>
-              <button className="py-[10px] desktop:py-[20px] px-[20px] desktop:px-[40px] bg-slate-200 rounded-[10px] ml-[5px] border-[2px] border-fifth hover:bg-fifth duration-300 text-fifth hover:text-slate-200">
+              <button className="py-[10px] desktop:py-[20px] px-[20px] desktop:px-[40px] bg-slate-200 rounded-[15px] ml-[5px] border-[2px] border-fifth hover:bg-fifth duration-300 text-fifth hover:text-slate-200">
                 <h1>Try it for Free</h1>
               </button>
             </div>
@@ -38,7 +38,11 @@ function Home() {
               kids using Augmented Reality
             </p>
           </div>
-          <div className="ml-[20px] mt-[20px] bg-secondary rounded-l-[20px] flex items-center justify-center desktop:items-end desktop:py-[50px]">
+          <div
+            // data-aos="fade-left"
+            // data-aos-duration="1500"
+            className="ml-[20px] mt-[20px] bg-secondary rounded-l-[20px] desktop:rounded-l-[40] large:rounded-l-[50px] flex items-center justify-center desktop:items-end desktop:py-[50px]"
+          >
             <img
               src="/images/UI Screenshot.png"
               alt=""
